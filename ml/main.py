0cd185db28aea6d148ee6206ff0c8cfe1f223af9
Facundo Martin Scasso <facundomartinscasso@gmail.com>
2024-02-05 21:16:33 -0300

    change to 55x56 and add earlystopper

--- ml/main.py
+++ ml/main.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv("../.env", override=True)
 
 import os
 import yaml
@@ -23,8 +23,27 @@ from torch.nn import (
 )
 from data import DatasetClass, get_total_classes
 
+
+class EarlyStopper:
+    def __init__(self, patience=1, min_delta=0.0):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.min_validation_loss = float("inf")
+
+    def early_stop(self, validation_loss: float) -> bool:
+        if validation_loss < self.min_validation_loss:
+            self.min_validation_loss = validation_loss
+            self.counter = 0
+        elif (self.min_validation_loss + self.min_delta) < validation_loss:
+            self.counter += 1
+            return (self.counter >= self.patience)
+        return False
+
+
 # -------------
 
+print(os.environ["LABELS_FD"])
 os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 print("Model for:", os.environ['SELECTED_FD'])
 
@@ -36,22 +55,23 @@ with open("config.yaml") as f:
 
 TOTAL_CLASSES = get_total_classes()
 
-# Input image size is 55x57 and the number of channels is 3 (RGB)
+# Input image size is 55x56 and the number of channels is 3 (RGB)
 model = Sequential(
     Conv2d(3, 32, (3, 3), padding=1),  # 32 filters
     ReLU(),
-    MaxPool2d((2, 2)),  # Output size: 32x27x28
+    MaxPool2d((2, 2)),  # Output size: 32x27x28  (rounded down)
     Conv2d(32, 64, (3, 3), padding=1),  # 64 filters
     ReLU(),
     MaxPool2d((2,2)),  # Output size: 64x13x14
     Flatten(),  # Output size: 64*13*14
-    Linear(64*13*14, 128),  
+    Linear(64*13*14, 128),
     ReLU(),
     Linear(128, TOTAL_CLASSES),
 )
 
 optimizer = Adam(model.parameters(), lr=CFG["adam_lr"])
 criterion = CrossEntropyLoss()
+estop = EarlyStopper(patience=3, min_delta=-0.01)
 
 model = model.cuda()
 summary(model, (3, 55, 57), batch_size=CFG["batch_size"], device="cuda")
@@ -106,11 +126,14 @@ for epoch in range(1, CFG["epochs"] + 1):
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
+        val_acc_pp = 100.0 * correct / total
         print(
             f"Epoch [{epoch}/{CFG['epochs']}],",
             f"Loss: {loss.item():.4f},",
-            f"Validation Accuracy: {100 * correct / total:.2f}%"
+            f"Validation Accuracy: {val_acc_pp:.2f}%"
         )
+        if estop.early_stop(100.0 - val_acc_pp):
+            break
 
 print("Training complete.")
 save(model, os.path.join("models", f"{os.environ['SELECTED_FD']}.pt"))
--- ml/predict.py
+++ ml/predict.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv("../.env", override=True)
 
 import os
 import yaml
